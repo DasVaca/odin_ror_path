c6c060d54b2af7c3a4e88795f8fd373472e2e090
DasVaca <sebastian23082000@gmail.com>
2021-08-16 15:17:23 -0400

    tic_tac_toe: basic game class and fixed bug on winner

--- ruby_programming/tic_tac_toe.rb
+++ ruby_programming/tic_tac_toe.rb
@@ -29,9 +29,9 @@ class Board
   end
 
   def win_diagonally
-    won_main = [board[0][0], board[1][1]].include? board[2][2]
-    won_alte = [board[0][2], board[1][1]].include? board[2][0]
-    board[1][1] != EMPTY && (won_main || won_alte) ? board[1][1]: nil
+    won_main = [board[0][0], board[1][1]].all? { |c| c ==  board[2][2] }
+    won_alte = [board[0][2], board[1][1]].all? { |c| c ==  board[2][0] }
+    (board[1][1] != EMPTY && (won_main || won_alte)) ? board[1][1]: nil
   end
 
   def win_vertically
@@ -40,7 +40,7 @@ class Board
     for i in 0...3
       next if board[0][i] == EMPTY
 
-      if [board[0][i], board[1][i]].include? board[2][i]
+      if [board[0][i], board[1][i]].all? { |c| c == board[2][i] }
         winner_symb = board[0][i]
         break
       end
@@ -70,3 +70,70 @@ class Board
 
 end
 
+
+class Game
+
+  attr_reader :board, :players
+
+  def initialize
+    @board = Board.new
+    @players = []
+  end
+
+  def play
+    2.times do |i|
+      puts "Register player #{i+1}"
+      register_player
+    end
+    
+    turn = 0
+
+    loop do
+      break if board.winner?
+
+      curr_player = players[turn % 2]
+      
+      puts "#{curr_player[:name]} is playing with symbol #{curr_player[:symbol]}."
+      board.show
+
+      print "Position to mark: "
+      position = gets.chomp.to_i
+
+      board.mark_cell(position, curr_player[:symbol])
+
+      turn += 1
+    end
+
+    puts "Game ended."
+  end
+
+  def get_player
+    print "Name: "
+    name = gets.chomp
+    
+    print "Symbol: "
+    symbol = gets.chomp
+
+    {name: name, symbol: symbol}
+  end
+
+  def symbol_taken? (symbol)
+    return false if players.size == 0
+
+    players[0][:symbol] == symbol
+  end
+
+  def register_player
+    new_player = nil
+    loop do 
+      new_player = get_player
+
+      break if not symbol_taken? new_player[:symbol]
+
+      puts "Symbol already taken. Please try again with a different symbol."
+    end
+    players << new_player 
+  end
+end
+
+(Game.new).play
