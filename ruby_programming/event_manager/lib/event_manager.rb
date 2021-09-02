3d76dad6cb28c7fada389e482c02deebcfa27497
DasVaca <sebastian23082000@gmail.com>
2021-09-02 14:15:59 -0400

    event_manager: frequent days, end of -boring- project

--- ruby_programming/event_manager/lib/event_manager.rb
+++ ruby_programming/event_manager/lib/event_manager.rb
@@ -48,6 +48,15 @@ def get_date(datestr)
   Time.strptime(datestr, "%Y/%d/%m %k:%M") {|y| y + 2000}
 end
 
+def hot_keys dict
+  dict.sort_by { |k, v| v }.reverse.map {|k, v| k}
+end
+
+def count_date(date, reg_hours, reg_days)
+  reg_hours[date.hour] += 1
+  reg_days[date.strftime("%A")] += 1
+end
+
 puts "EventManager initialized"
 
 contents = CSV.open(
@@ -60,6 +69,7 @@ template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
 reg_hours = Hash.new(0)
+reg_days = Hash.new(0)
 
 contents.each do |row|
   #id = row[0]
@@ -67,12 +77,15 @@ contents.each do |row|
   #phonenumber = clean_phonenumber row[:homephone]
   #zipcode = clean_zipcode(row[:zipcode])
   #legislators = legislators_by_zipcode(zipcode)
-  reg_hours[get_date(row[:regdate]).hour] += 1
+  count_date(get_date(row[:regdate]), reg_hours, reg_days)
+  
   #form_letter = erb_template.result(binding) 
 
   #save_letter(id, form_letter)
 end
 
-hot_hours = reg_hours.sort_by { |k, v| v }.reverse.map {|hour, count| hour}
+hot_hours = hot_keys reg_hours
+puts "The 3 best hours for advertisement are #{hot_hours.first(3).join(', ')}."
 
-puts "The 3 best hours for advertisement are #{hot_hours.first(3)}"
+hot_days = hot_keys reg_days 
+puts "The 3 days where ppl register are #{hot_days.first(3).join(', ')}."
