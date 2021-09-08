e24594d642dce81546f3e08396e4a9f634ad6fb3
DasVaca <sebastian23082000@gmail.com>
2021-09-08 15:52:04 -0300

    custom_enumerable: some methods
    each
    each_with_index
    all?
    any?
    none?
    count

---
+++ ruby_programming/custom_enumerable/custom_enumerable.rb
@@ -0,0 +1,79 @@
+module Enumerable
+=begin
+I did not wanted to make overcalls to others methods 
+(ie. calling my_select from my_all?).
+So there's not overcharge and hopefully best performance.
+=end
+
+  def crash? bg
+    raise ArgumentError.new("No block given.") unless bg
+  end
+
+  def my_each
+    crash? block_given?
+
+    for item in self
+        yield item
+    end
+
+    self
+  end
+
+  def my_each_with_index
+    crash? block_given?
+    
+    i = 0
+    
+    self.my_each do |item|
+      yield item, i
+      i += 1
+    end
+  end
+
+  def my_select
+    crash? block_given?
+    selected = []
+    
+    self.my_each do |item|
+      selected << item if yield item
+    end
+
+    selected
+  end
+  
+  def my_all?
+    crash? block_given?
+    
+    for item in self
+      return false unless yield item
+    end
+
+    return true
+  end
+
+  def my_any?
+    crash? block_given?
+    
+    for item in self
+      return true if yield item
+    end
+
+    return false
+  end
+
+  def my_none?
+    crash? block_given?
+    # Here I did call other methods, just to show the alternative idea
+    self.my_select { |item| yield item}.size == 0
+  end
+
+  def my_count(it = nil)
+    if it
+      self.my_select {|item| item == it}.size
+    elsif block_given?
+      self.my_select {|item| yield item}.size
+    else
+      self.size
+    end
+  end
+end
