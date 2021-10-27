2c52115148fdd682c3421e17c3a1f70c416dbb7e
DasVaca <sebastian23082000@gmail.com>
2021-10-27 20:37:14 -0300

    ruby: bst -> iterative insert method

--- ruby_programming/data_structures/bst.rb
+++ ruby_programming/data_structures/bst.rb
@@ -3,12 +3,12 @@ class Node
   include Comparable
 
   attr_reader :data
-  attr_accessor :left_node, :right_node
+  attr_accessor :left, :right
 
   def initialize(data)
     @data = data
-    @left_node = nil
-    @right_node = nil
+    @left = nil
+    @right = nil
   end
 end
 
@@ -20,25 +20,61 @@ class Tree
 
   def build_tree(array)
     return nil unless array
-    array = array.uniq.sort
-    build_tree_recursive(array)
+    build_tree_recursive(array.uniq.sort)
   end
 
   def build_tree_recursive(array)
     return nil if array.size == 0 
-    p array
     root = Node.new(array[array.size / 2])
-    root.left_node  = build_tree_recursive(array[...array.size/2])
-    root.right_node = build_tree_recursive(array[array.size/2 + 1..])
+    root.left  = build_tree_recursive(array[...array.size/2])
+    root.right = build_tree_recursive(array[array.size/2 + 1..])
     root
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
-    pretty_print(node.right_node, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_node
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
-    pretty_print(node.left_node, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_node
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end 
+
+  def find(data, root=@root)
+    return nil unless root
+
+    if root.data == data
+      root
+    elsif data < root.data
+      self.find(data, root=root.left)
+    else
+      self.find(data, root=root.right)
+    end
+  end
+
+  def insert(data)
+    root = @root
+    loop do
+      break if data == root.data
+
+      if data < root.data
+        if root.left
+          root = root.left
+        else
+          root.left = Node.new(data)
+          break
+        end
+      else
+        if root.right
+          root = root.right
+        else
+          root.right = Node.new(data)
+          break
+        end
+      end
+    end
+  end
 end
 
-a = Array.new(15) { rand(1..100) }
-Tree.new(a).pretty_print
+a = [1, 2, 4, 5, 8, 9, 10]
+t = Tree.new(a)
+t.pretty_print
+t.insert(3)
+t.pretty_print
